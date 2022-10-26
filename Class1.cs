49f8ced02dc757bf9f9de17f5c0ef1c91f11c6b7
ashwin333 <ashwinpatil832@gmail.com>
2022-10-26 13:04:33 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter Percentage");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >75)
+            {
+                Console.WriteLine($"(n) A Grade");
+            }
+            else
+            {
+                if (n >= 61 && n <= 75)
+                {
+                    Console.WriteLine($"(n) B Grade");
+                }
+
+                {
+                    if (n >= 40 && n <= 60)
+                    {
+                        Console.WriteLine($"(n) C Grade");
+
+                    }
+
+                    {
+                        Console.WriteLine($"(n) Fail");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            int n =Convert.ToInt32(Console.ReadLine());
+            if (n >= 0)
+            {
+                if (n== 0) 
+                {
+                    Console.WriteLine(" Enter number is zero");
+                }
+                else
+                {
+                    Console.WriteLine(" Enter number  is positive");
+                }
+
+                {
+                    Console.WriteLine("Enter  number is negative");
+                }
+            }
+        }
+    }
+}
